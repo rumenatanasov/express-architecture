d1a0fc18c752b2803410daea7106729a83598227
Rumen Atanasov <atanasovrumen123@gmail.com>
2017-02-20 11:49:16 +0200

    added intellisence

---
+++ typings/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="globals/mongoose/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="modules/express-serve-static-core/index.d.ts" />
+/// <reference path="modules/express/index.d.ts" />
+/// <reference path="modules/serve-static/index.d.ts" />
