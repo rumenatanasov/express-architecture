8f7ec82b1bccfab5f258ae6f38dca0487908d88a
Rumen Atanasov <rumen.atanasov29>
2017-02-20 12:29:31 +0200

    init skeleton

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+let express = require('express')
+let mongoose = require('mongoose')
+let app = express()
 
+app.get('/', (req, res) => {
+    mongoose.connect('mongodb://localhost:27017/express-architecture')
+
+    console.log('MongoDB is ready!')
+    res.send('Cool')
+})
+app.listen(1961)
+console.log('Express ready')
