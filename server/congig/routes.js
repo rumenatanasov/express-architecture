e324d5c65fbc9c4bc74cfdc39367a1c10f380d85
Rumen Atanasov <rumen.atanasov29>
2017-02-20 18:58:26 +0200

    save the username if passwords do not match

--- server/congig/routes.js
+++ server/congig/routes.js
@@ -4,6 +4,7 @@ module.exports = (app) => {
   app.get('/', controllers.home.index)
   app.get('/about', controllers.home.about)
   app.get('/users/register', controllers.users.register)
+  app.post('/users/create', controllers.users.create)
 
   app.all('*', (req, res) => {
     res.status(404)
