5d341704ca0e9dc885b897f85b0653a8799e5cca
Rumen Atanasov <rumen.atanasov29>
2017-02-21 13:43:32 +0200

    authentication added

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,6 +1,8 @@
 let homeController = require('./home-controllers')
 let usersController = require('./users-controller')
+let articleController = require('./articles-controller')
 module.exports = {
     home: homeController,
-    users: usersController
+    users: usersController,
+    articles: articleController
 }
